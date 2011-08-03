54e156902d273acd6d55303d5485fb3ac4a18e14
kejing <806328591@qq.com>
2011-08-03 10:46:18 +0800

    international by kejing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_i18n_locale_from_params
   before_filter :authorize
   protect_from_forgery
-  
+
   private
-  
+
     def current_cart
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
@@ -11,14 +12,37 @@ class ApplicationController < ActionController::Base
       session[:cart_id] = cart.id
       cart
     end
-    
   protected
-   
-     def authorize
-      unless User.find_by_id(session[:user_id])
-        redirect_to login_url, :notice => "Please log in"
-        # flash[:notice] = "Please log in"
-        # redirect_to :controller => 'admin', :action => 'login'
+#    def authorize
+#      unless User.find_by_id(session[:user_id])
+#        redirect_to login_url, :notice => "Please log in"
+#      end
+#    end
+    def authorize
+      if request.format == Mime::HTML 
+        unless User.find_by_id(session[:user_id])
+          redirect_to login_url, :notice => "Please log in"
+        end
+      else
+        authenticate_or_request_with_http_basic do |username, password|
+          User.authenticate(username, password)
+        end
       end
+    end
+    
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = 
+            "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { :locale => I18n.locale }
     end
 end

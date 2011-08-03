ee5c2e9709fa5c9081a448fe454df4f755d57180
daiyuqiang <250936666@qq.com>
2011-08-03 09:45:00 +0800

    ajax by daiyuqiang

--- db/migrate/20110719072504_combine_items_in_cart.rb
+++ db/migrate/20110719072504_combine_items_in_cart.rb
@@ -19,7 +19,7 @@ class CombineItemsInCart < ActiveRecord::Migration
 
   def self.down
     # split items with quantity>1 into multiple items
-    LineItem.where("quantity>a").each do |lineitem|
+    LineItem.where("quantity>1").each do |lineitem|
       # add individual items
       lineitem.quantity.times do
         LineItem.create :cart_id=>lineitem.cart_id, :product_id=>lineitem.product_id, :quantity=>1
